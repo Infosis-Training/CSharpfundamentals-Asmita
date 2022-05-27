98b5d5f71809a586624b4bfda6c3013fabcf22ab
Asmita Gautam <asmitagautam2124@gmail.com>
2022-05-27 07:56:07 +0545

    csharp datatype intro

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// datatype:
+// 1-numbers 
+// ranges from
+ byte a = 0 to 255;
+ short b = 3534;
+ int c = 2523523;
+ long d = 23232525235;
+ float e = 2342.546f;
+ double f = 23423.456;
+ decimal g = 34.34535353555355555333m;
+
+// 2-boolean
+// true or false
+bool h = false;
+
+// 3-character and string
+char i = 'A';
+string j = "Hello";
+
+// 4-nullable type
+int? value = null; 
+
