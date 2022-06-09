af408a18a133c2dac4e388ac65478e51f3bfb2a2
Asmita Gautam <asmitagautam2124@gmail.com>
2022-06-09 07:51:51 +0545

    Worked On MovieManagement

--- WebDevelopment/MovieManagement/MovieManagement/Controllers/MovieController.cs
+++ WebDevelopment/MovieManagement/MovieManagement/Controllers/MovieController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieManagement.Models;
+using System.Collections.Generic;
 
 namespace MovieManagement.Controllers
 {
@@ -6,7 +8,46 @@ namespace MovieManagement.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            //movies.Add(new Movie()
+            //{
+            //    Name = "3 Idiots",
+            //    Code = "A",
+            //    Genre = "Comedy",
+            //    Description = "Movie Based on Real Story",
+            //    LengthInMin = 178,
+            //    ReleaseDate = DateTime.Parse("2015-01-01")
+            //});
+            Movie movie1 = new()
+            {
+                Name = "3 Idiots",
+                Code = "A",
+                Genre = "comedy",
+                Description = "movie based on real story",
+                LengthInMin = 178,
+                ReleaseDate = DateTime.Parse("2015-01-01")
+            };
+            Movie movie2 = new()
+            {
+                Name = "KGF",
+                Code = "B",
+                Genre = "Action Drama",
+                Description = "movie based on gold smuggling",
+                LengthInMin = 188,
+                ReleaseDate = DateTime.Parse("2020-01-01")
+            };
+            Movie movie3 = new()
+            {
+                Name = "KGF 2",
+                Code = "A",
+                Genre = "Action Drama",
+                Description = "movie based on gold smuggling",
+                LengthInMin = 198,
+                ReleaseDate = DateTime.Parse("2022-01-01")
+            };
+            List<Movie> movies = new(){movie1, movie2, movie3};
+
+            return View(movies);
         }
     }
 }
+
--- WebDevelopment/MovieManagement/MovieManagement/Models/Movie.cs
+++ WebDevelopment/MovieManagement/MovieManagement/Models/Movie.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Code { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
         public DateTime ReleaseDate { get; set; }
         public float LengthInMin { get; set; }
 
